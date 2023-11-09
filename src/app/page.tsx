73faa9683321f7a1dec911826e16a4f6b7d7bc8f
Lucas Nhimi <lucasnhimi@gmail.com>
2023-11-09 08:07:40 -0300

    Cupom descount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,25 @@ import FaqSection from './_components/faq-section';
 
 export default function Home() {
   return (
-    <main className=''>
-      <IntroSection />
-      <AboutSection />
-      <FeatureSection />
-      <InvestmentSection />
-      <FaqSection />
-    </main>
+    <>
+      <div className='fixed h-12 p-4 flex justify-center items-center group bg-gradient-to-r from-primary to-secondary w-full z-10'>
+        <p className='text-white'>
+          Utilize o cupom{' '}
+          <span className='font-mono font-bold'>COMUNIDADETND</span> e ganhe 50%
+          de desconto. Valor à vista de{' '}
+          <span className='font-mono font-bold'>
+            R$ 489,00 ou 12x de R$ 48,81
+          </span>{' '}
+          .
+        </p>
+      </div>
+      <main className=''>
+        <IntroSection />
+        <AboutSection />
+        <FeatureSection />
+        <InvestmentSection />
+        <FaqSection />
+      </main>
+    </>
   );
 }
